cc67e71f0e92884014f1b9244a31df1b7f956a98
afloresep <41540492+afloresep@users.noreply.github.com>
2018-07-24 11:02:27 +0200

    Update signal_sine_plot.py

--- signal_sine_plot.py
+++ signal_sine_plot.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 A= input("Please, introduce the amplitude of the signal  ")
 T= input("Please, introduce the period  ")
 L= input("Please, introduce the lenght  ")
-i= np.arange(0,L,T)
-xt= A*np.sin((2*np.pi)/i)
-print xt
-plt.plot(x,s)
+i= np.arange(T,L,T)
+x= A*np.sin((2*np.pi)/i)
+print x
+plt.plot(i,x)
 plt.title('TIME KNOWN SIGNAL')
 plt.ylabel("x(t)")
 plt.xlabel("time(s)")
