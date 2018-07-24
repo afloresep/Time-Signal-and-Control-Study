5c2fba0336c691a0577148ada60fa1b9319f830b
afloresep <41540492+afloresep@users.noreply.github.com>
2018-07-24 18:56:13 +0200

    Update signal_sine_plot.py

--- signal_sine_plot.py
+++ signal_sine_plot.py
@@ -2,14 +2,18 @@
 # coding=utf-8
 import numpy as np
 import matplotlib.pyplot as plt
+
 A= input("Please, introduce the amplitude of the signal  ")
 T= input("Please, introduce the period  ")
+f= input("Please, introduce the frequency ")
 L= input("Please, introduce the lenght  ")
+
 i= np.arange(T,L,T)
-x= A*np.sin((2*np.pi)/i)
-print x
+x= A*np.sin((2*np.pi*f)*i)
 plt.plot(i,x)
-plt.title('TIME KNOWN SIGNAL')
-plt.ylabel("x(t)")
-plt.xlabel("time(s)")
+plt.ylabel('x(t)')
+plt.xlabel('t(s)')
+plt.title('Known time signal ')
 plt.show()
+
+#creo que estoy operando mal, el resultado es parecido al de una sinusoide pero no es curva
